4b4f649d81e494c7d40d5d71a41c6d06138d3366
wajidwebd <wajid.webd@gmail.com>
2024-11-10 13:28:41 +0530

    first

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+// next.config.mjs
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverActions: true,  // Adjust based on your needs
+  },
+};
+
+export default nextConfig;  // Use export default
